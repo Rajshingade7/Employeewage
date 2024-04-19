89d4892ee692c621c1c065dae55680073662f9c9
Raj143 <rajshingade777@gmail.com>
2024-04-19 16:24:26 +0530

    [raj] add : scoping of variables

---
+++ PracticeFolder/_03Scoping.js
@@ -0,0 +1,15 @@
+// Scoping in JavaScript refers to the set of rules that determine where variables and functions are accessible within your code. It defines the visibility and accessibility of identifiers (such as variables and functions) in different parts of your code.
+
+// JavaScript has two main types of scope:
+
+// Global Scope:
+// Variables declared outside of any function or block have global scope.
+// Global variables are accessible from anywhere within your JavaScript code, including inside functions and blocks.
+// Local Scope:
+// Variables declared inside a function or block have local scope.
+// Local variables are only accessible within the function or block in which they are declared.
+// Variables declared with var keyword are function-scoped, meaning they are accessible throughout the function in which they are declared.
+
+// Variables declared with let and const keywords, introduced in ES6, are block-scoped, meaning they are accessible only within the block (enclosed by curly braces) in which they are declared.
+
+// Understanding scoping is crucial for writing maintainable and bug-free JavaScript code, as it helps prevent naming conflicts and unintended variable access. It's essential to be aware of scoping rules to write clear and organized code.
