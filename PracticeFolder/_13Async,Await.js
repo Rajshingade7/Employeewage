2d04520aa6a5e3e15814c1d90e3683ed9e4d4c84
Raj143 <rajshingade777@gmail.com>
2024-04-20 15:45:38 +0530

    changed a little bit

--- PracticeFolder/_13Async,Await.js
+++ PracticeFolder/_13Async,Await.js
@@ -4,18 +4,19 @@
 // The async keyword is used to declare an asynchronous function, which allows you to use the await keyword inside it.
 // An async function always returns a promise. If the function returns a value, the promise will be resolved with that value. If the function throws an exception, the promise will be rejected with the thrown value.
 
-async function fetchData() {
-    return 'Data fetched!';
-}
+// async function fetchData1() {
+//     return 'Data fetched!';
+// }
 
-fetchData().then(data => {
-    console.log(data); // Output: Data fetched!
-});
+// fetchData1().then(data => {
+//     console.log(data); // Output: Data fetched!
+// });
 
 async function fetchData() {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve('fetch Data !');
+            
         }, 1000);
     });
 }
@@ -31,3 +32,4 @@ async function displayData() {
 // Here in this example firstly fetchdata function is resolved completely and then and then only new console line is printed 
 // Here with the help of await keyword we can wait for the promise to be resolved(means fetchdata function) and then we can use the data.
 displayData();
+
