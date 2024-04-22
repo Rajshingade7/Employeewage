2013aa6482e63c9cb9b9e4ef1925b6c43abc368e
Raj143 <rajshingade777@gmail.com>
2024-04-22 12:17:29 +0530

    [Raj] add : ADded event looping statement

---
+++ PracticeFolder/_21EventLoop.js
@@ -0,0 +1,18 @@
+// Event Queue 
+
+console.log("Start");
+
+setTimeout(function() {
+  console.log("Inside setTimeout");
+}, 0);
+
+console.log("End");
+
+
+// Start
+// End
+// Inside setTimeout
+
+
+
+// Event Loop:
