d118c0af86b321b931dbebfb5c9f51dd4980aaf7
Raj Shingade <97889220+Rajshingade7@users.noreply.github.com>
2024-04-24 11:31:00 +0530

    Update _18Closure.js

--- PracticeFolder/_18Closure.js
+++ PracticeFolder/_18Closure.js
@@ -14,3 +14,5 @@ function outerFunction() {
 //   Jaise, agar aap ek outer function mein ek counter variable define karte hain aur uske andar ek inner function hai jo us counter ko badha kar return karta hai, toh jab aap us inner function ko baad mein bhi call karte hain, toh wo us counter ke value ko yaad rakhta hai jaise ek closure. Isse, aap multiple instances of that inner function create kar sakt
 
 // e hain, har ek ka apna counter value rakh kar.
+
+// Matlab agar ek function mein doosri function define hai aur uss doosri function mein uss pehli function ke variables ko access kiya jaata hai, toh yeh concept of closure use hota hai.
